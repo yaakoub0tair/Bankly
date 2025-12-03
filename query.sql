ab049fc0c9f5a61ebe69785a57d7cd57bc19a59a
Yaakoub tair <yaakoubt05@gmail.com>
2025-12-03 16:29:20 +0100

    Modifier le numéro de téléphone d’un customer

--- query.sql
+++ query.sql
@@ -1,2 +1,4 @@
 INSERT INTO Customers (full_name,email,phone,registraction_date)
 VALUES('Mohamed lfrah','lfrah@gmail.com','0712345678','2025-01-10 10:00:00');
+
+UPDATE Customers SET phone = '0600000000' WHERE full_name='Mohamed lfrah';
